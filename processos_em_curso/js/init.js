ff2f841dca9761b2153d79d1d456bd8beb8d0274
rpcavaco <rpcavaco@gmail.com>
2021-03-03 14:31:25 +0000

    Inserção esriConfig e WebMap

--- processos_em_curso/js/init.js
+++ processos_em_curso/js/init.js
@@ -1,11 +1,21 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
-	var map = new Map({
-	  basemap: "topo-vector"
-	});
-	var view = new MapView({
-	  container: "viewDiv", // Reference to the view div created in step 5
-	  map: map, // Reference to the map object created before the view
-	  zoom: 4, // Sets zoom level based on level of detail (LOD)
-	  center: [15, 65] // Sets center point of view using longitude,latitude
-	});
-  });
+require([
+	"esri/config",
+	"esri/WebMap",
+	"esri/views/MapView"
+	], function(
+		esriConfig, 
+		WebMap,
+		MapView
+	) {
+		esriConfig.portalUrl = "https://portalsig.cm-porto.pt/arcgis";
+
+		const webmap = new WebMap({
+		  portalItem: { // autocasts as new PortalItem()
+			id: "343749483d5d4c86b417a8b6f0aec18d" // Licenciamento em curso
+		  }
+		});
+		var view = new MapView({
+			container: "viewDiv", // Reference to the view div created in step 5
+			map: webmap // Reference to the map object created before the view
+		});
+  	});
